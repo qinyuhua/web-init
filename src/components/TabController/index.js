97707eb657aa19963d3c7be33d0b23c71b961564
yuhua_qin@xinyan.com <yuhua_qin@xinyan.com>
2019-11-11 19:48:02 +0800

    1

---
+++ src/components/TabController/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Tabs } from 'antd';
+
+const { TabPane } = Tabs;
+
+class TabController extends Component {
+  /**
+   * props
+   * @param props
+   * 至少包含 导航栏、内容
+   */
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    const { panes, propsData, dropdown, activeKey } = this.props;
+
+    return (
+      <div>
+        <Tabs
+          hideAdd
+          onChange={this.onChange}
+          activeKey={activeKey}
+          type="editable-card"
+          onEdit={this.onEdit}
+          // className={`${styles.tabBar} ant-page${activeKey.split('/').join('-')}`}
+          tabBarGutter={4}
+          tabBarExtraContent={dropdown}
+        >
+          {panes
+            ? panes.map(pane => (
+                <TabPane tab={pane.title} key={pane.path} closable={pane.isCurrnet}>
+                  <pane.component
+                    addTabs={this.addTabs}
+                    removeTabs={this.remove}
+                    tabsData={pane.tabsData}
+                    {...propsData}
+                  />
+                </TabPane>
+              ))
+            : null}
+        </Tabs>
+      </div>
+    );
+  }
+}
+
+export default TabController;
